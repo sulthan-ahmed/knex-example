55b2071432502619d4954fd1fcb09a51915bb713
sulthan.ahmed <sulthan.ahmed@digital.homeoffice.gov.uk>
2021-07-13 21:57:34 +0100

    Add router so the routing is more neater

--- index.js
+++ index.js
@@ -2,20 +2,12 @@ require('dotenv').config()
 const express = require('express');
 const router = require('./routes')
 const PORT = process.env.PORT || 5000;
-const config = require('./knexfile')[process.env.NODE_ENV || 'development'];
-const knex = require('knex')(config);
 
 const app = express();
 app.use(express.json());
+app.use(router);
 
 app.use(express.urlencoded({ extended: false }));
-
-app.get('/todos', (req, res) => {
-  knex.raw('select * from todos').then(todos => {
-    res.send(todos.rows)
-  })
-})
-
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`)
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const config = require('../knexfile')[process.env.NODE_ENV || 'development'];
+const knex = require('knex')(config);
+
+router.get('/todosallraw', (req, res) => {
+  knex.raw('select * from todos').then(todos => {
+    // to filter, you can call rows
+    res.send(todos.rows)
+  })
+})
+
+router.get('/todosallknexshortcut', (req, res) => {
+  knex.select().from('todos').then(todos => {
+    // don't put rows because it's already filtered
+    res.send(todos)
+  })
+})
+
+router.get('/todosoneraw', (req, res) => {
+  knex.raw('select * from todos where id = 1')
+    .then(todos => {
+      res.send(todos.rows)
+    })
+})
+
+router.get('/todosoneknexshortcut', (req, res) => {
+  knex.select().from('todos').where('id', 1)
+    .then(todos => {
+      // don't put rows because it's already filtered
+      res.send(todos)
+    })
+})
 
 module.exports = router;
