6101f93d4ce4e576c8493a6c1ac1782656590c11
sulthan.ahmed <sulthan.ahmed@digital.homeoffice.gov.uk>
2021-07-14 11:40:31 +0100

    knex update example and use req.params and req.body

--- routes/index.js
+++ routes/index.js
@@ -24,8 +24,11 @@ router.get('/todosoneraw', (req, res) => {
     })
 })
 
-router.get('/todosoneknexshortcut', (req, res) => {
-  knex.select().from('todos').where('id', 1)
+// passing in the value
+router.get('/todosoneknexshortcut/:id', (req, res) => {
+  knex.select()
+      .from('todos')
+      .where('id', req.params.id)
     .then(todos => {
       // don't put rows because it's already filtered
       res.send(todos)
@@ -58,5 +61,49 @@ router.post('/todosknexshortcut', (req, res) => {
     })
     .catch((err) => console.error(err))
 })
+
+router.post('/todos', (req, res) => {
+  knex('todos').insert({
+    id: req.body.id,
+    title: req.body.title,
+    user_id: req.body.user_id
+  })
+    .then(() => {
+      knex.select().from('todos')
+        .then(users => {
+          res.send(users)
+        })
+    })
+    .catch((err) => console.error(err))
+})
+
+router.put('/todosupdateraw/:id', (req, res) => {
+  knex.raw(`update todos set ${req.body.field} = ? where id = ?`, [req.body.value, req.params.id])
+    .then(() => {
+      knex.select().from('todos')
+        .then(users => {
+          res.send(users)
+        })
+    })
+    .catch((err) => console.error(err))
+})
+
+router.put('/todosupdate/:id', function(req, res) {
+  console.log("req body =>", req.body)
+  knex('todos').where('id', req.params.id)
+              .update({
+                title: req.body.title,
+                completed: req.body.completed
+              })
+              .then(function() {
+                knex.select()
+                    .from('todos')
+                    .then(function(todos) {
+                      res.send(todos);
+                    })
+              })
+            .catch((err) => console.error(err))
+})
+
 
 module.exports = router;
